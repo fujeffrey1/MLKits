75b70813b41063c90949b2eec7acbc98c2ea5596
603156 <jeffreyfu@C02Y70APJGH7.fios-router.home>
2020-07-21 15:18:25 -0400

    knn final

---
+++ knn-tf/index.js
@@ -0,0 +1,38 @@
+require("@tensorflow/tfjs-node");
+const tf = require("@tensorflow/tfjs");
+const loadCSV = require("./load-csv");
+
+function knn(features, labels, predictionPoint, k) {
+    const { mean, variance } = tf.moments(features, 0);
+    const scaledPrediction = predictionPoint.sub(mean).div(variance.pow(0.5));
+
+    return (
+        features
+            .sub(mean)
+            .div(variance.pow(0.5))
+            .sub(scaledPrediction)
+            .pow(2)
+            .sum(1)
+            .pow(0.5)
+            .expandDims(1)
+            .concat(labels, 1)
+            .unstack()
+            .sort((a, b) => (a.get(0) > b.get(0) ? 1 : -1))
+            .slice(0, k)
+            .reduce((acc, pair) => acc + pair.get(1), 0) / k
+    );
+}
+
+let { features, labels, testFeatures, testLabels } = loadCSV("kc_house_data.csv", {
+    shuffle: true,
+    splitTest: 10,
+    dataColumns: ["lat", "long", "sqft_lot", "sqft_living"],
+    labelColumns: ["price"],
+});
+
+features = tf.tensor(features);
+labels = tf.tensor(labels);
+
+const result = knn(features, labels, tf.tensor(testFeatures[0]), 10);
+const err = (testLabels[0][0] = result) / testLabels[0][0];
+console.log("err", err * 100);
