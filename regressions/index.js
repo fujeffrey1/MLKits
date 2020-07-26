ecbb8694e7215cf64373dd241315ad92414ea679
603156 <jeffreyfu@C02Y70APJGH7.fios-router.home>
2020-07-25 23:26:23 -0400

    finish linear regression with plot

--- regressions/index.js
+++ regressions/index.js
@@ -1,12 +1,35 @@
 require("@tensorflow/tfjs-node");
 const tf = require("@tensorflow/tfjs");
 const loadCSV = require("./load-csv");
+const LinearRegression = require("./linear-regression");
+const plot = require("node-remote-plot");
 
 let { features, labels, testFeatures, testLabels } = loadCSV("./cars.csv", {
     shuffle: true,
     splitTest: 50,
-    dataColumns: ["horsepower"],
+    dataColumns: ["horsepower", "weight", "displacement"],
     labelColumns: ["mpg"],
 });
 
-console.log(testLabels);
+const regression = new LinearRegression(features, labels, {
+    learningRate: 0.1,
+    iterations: 100,
+});
+
+regression.train();
+const r2 = regression.test(testFeatures, testLabels);
+
+plot({
+    x: regression.mseHistory.reverse(),
+    xLabel: "Iteration #",
+    yLabel: "Mean Squared Error",
+});
+
+// plot({
+//     x: regression.bHistory,
+//     y: regression.mseHistory.reverse(),
+//     xLabel: "Value of B",
+//     yLabel: "Mean Squared Error",
+// });
+
+console.log("r2", r2);
